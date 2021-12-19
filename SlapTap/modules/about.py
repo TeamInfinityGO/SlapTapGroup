2a37e16c4a5ed82b92cf122cb500dd274fc31c44
TeamInfinityGO <95580943+TeamInfinityGO@users.noreply.github.com>
2021-12-19 14:11:27 +0530

    Add files via upload

---
+++ SlapTap/modules/about.py
@@ -0,0 +1,14 @@
+__help__ = """
+** Anki Vector - A Powerful Telegram Group Manager 🎶 **
+
+Powerful Abilities
+
+• Group Voice Chat Music Play ❤️
+• File To Link and URL Upload 📂
+• Youtube Downloader 🎵
+
+Developers [TeamAnkiVector🇱🇰](https://t.me/TeamAnkiVector)
+
+**~ @TheAnkiVectorBot**
+"""
+__mod_name__ = "About"
